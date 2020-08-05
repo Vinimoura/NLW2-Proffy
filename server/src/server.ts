8385df2ce4c3ebe4dc0dc9eac3ab409143302b8f
Adriana Lima <adrianalimafm@gmail.com>
2020-08-04 23:31:05 -0300

    Update server

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,16 @@
 import express from 'express';
+import cors from 'cors';
+
+import routes from './routes';
 
 const app = express();
 
-app.listen(3333);
+app.use(cors());
+
+app.use(express.json());
+
+app.use(routes);
+
+app.listen('3333', () => {
+  console.log('> Servidor rodando em http://localhost:3333');
+});
